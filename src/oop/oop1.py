cb6443a091055d28a09f1112c451aba849944999
Jonathan Daly <dalyj138@gmail.com>
2020-02-22 12:38:41 -0500

    Implemented class stubs. Currently not printing out drive info correctly.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,20 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Vehicle is the base class that all other classes inherit from.
+class Vehicle:
+    def __init__(self, num_wheels):
+        self.num_wheels = num_wheels
+
+
+# FlightVehicle is a child class of Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# Starship inherits from FlightVehicle
+
+
+class Starship(FlightVehicle):
+    pass
